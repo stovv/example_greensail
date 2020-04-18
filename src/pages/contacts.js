d7708a9f2971f222641e42e5b5b6f08cb6be9b53
dstavrov <danila.stavrov@intel.com>
2020-04-18 13:58:08 +0300

    Example 18 aprill multiple cards, jsx arguments

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -5,30 +5,79 @@ import {Row, Col, Card} from 'react-bootstrap';
 // padding-top -> paddingTop
 
 
-const MyCard = ({headerText, children}) => (
-    <Card
-        bg="primary"
-        text='white'
-        style={{ width: '100%', height:"200px" }}>
-        <Card.Header>{headerText}</Card.Header>
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-            {children}
-        </Card.Text>
-        </Card.Body>
-    </Card>
-);
+const MyCard = ({headerText, children, small}) => {
 
+    if (small){
+        return(
+        <Card
+            bg="primary"
+            text='white'
+            style={{ width: '100%', height:"200px" }}>
+            <Card.Header>{headerText}</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    {children}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        );
+    }
+
+    return(
+        <Card
+            bg="primary"
+            text='white'
+            style={{ width: '100%', height:"200px" }}>
+            <Card.Header>{headerText}</Card.Header>
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    {children}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+// all === true ? console.log("Hello") : console.log("no");
+/*
+* if (all === true){
+*   console.log("Hello");
+* }
+* else{
+*   console.log("no");
+* }
+*
+* */
 
 class Contacts extends React.Component{
     render(){
+        const {all} = this.props;
+        const arr = [
+            {header: "Заголовок1", text: "text1", small: true},
+            {header: "Заголовок2", text: "text2", small: true},
+            {header: "Заголовок3", text: "text2"},
+            {header: "Заголовок4", text: "text4"},
+            {header: "Заголовок5", text: "text5", small: true},
+        ];
+
         return(
             <Row>
                 <Col md={3}>
-                    <MyCard headerText="Я заголовок">Текст внутри карточки</MyCard>
-                    <MyCard headerText="Я заголовок">Текст  карточки</MyCard>
-                    <MyCard headerText="Я ">Текст внутри </MyCard>
+                    {
+                        all
+                            ? <>
+                                {
+                                    arr.map((item, index)=>(
+                                        <MyCard headerText={item.header} small={item.small}>{item.text}</MyCard>
+                                    ))
+                                }
+                            </>
+                            : <>
+                                <MyCard headerText="Я заголовок">Текст  карточки</MyCard>
+                                <MyCard headerText="Я ">Текст внутри </MyCard>
+                            </>
+                    }
                 </Col>
             </Row>
         );
